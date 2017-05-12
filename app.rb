bedd57786fbb0959d6a05a2f44d91c2273e0c636
Sara Schultz <saschultz95@gmail.com>
2017-05-12 14:19:14 -0700

    add functionality in UI to update and delete a store in store.erb

--- app.rb
+++ app.rb
@@ -20,4 +20,23 @@ post '/add_store' do
   redirect '/add_store'
 end
 
+get '/store/:id' do
+  @store = Store.find(params['id'].to_i)
+  erb :store
+end
+
+patch '/store/:id/updated' do
+  store_id = params['id'].to_i
+  @store = Store.find(store_id)
+  store_name = params['store_name']
+  @store.update({:name => store_name})
+  redirect "/store/#{store_id}"
+end
+
+delete '/store/:id' do
+  store = Store.find(params['id'].to_i)
+  store.delete
+  redirect '/add_store'
+end
+
 ## brand paths
