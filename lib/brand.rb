d577239d638d905ece542e8e2ba936ee715e9dd3
Sara Schultz <saschultz95@gmail.com>
2017-05-12 11:00:42 -0700

    pass spec to validate that brand name input was not blank in brand_spec.rb

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
   before_save(:title_case)
-
+  validates(:name, :presence => true)
   def title_case
     self.name=(name.split(/(\W)/).map(&:capitalize).join)
   end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Brand do
   it { should have_and_belong_to_many(:stores) }
   it { is_expected.to callback(:title_case).before(:save)}
+
+  it 'validates presence of name in a brand' do
+    test_brand = Brand.create name: 'Toms'
+    expect(test_brand.update(name: ' ')).to(eq(false))
+  end
 end
