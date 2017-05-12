068674850fed3b280c5c8c7bd258c4b8b1594a2a
Sara Schultz <saschultz95@gmail.com>
2017-05-12 10:20:25 -0700

    pass spec to capitalize the title of a store in store_spec.rb

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
+  before_save(:title_case)
+
+  def title_case
+    self.name=(name.split(/(\W)/).map(&:capitalize).join)
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Store do
   it { should have_and_belong_to_many(:brands) }
+  it { is_expected.to callback(:title_case).before(:save)}
 end
