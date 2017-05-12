e370128befe9e217aca2caff82754515c14a45b2
Sara Schultz <saschultz95@gmail.com>
2017-05-12 16:27:29 -0700

    add to store_spec and pass validation test that store name is unique before it is saved

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -9,6 +9,12 @@ describe Store do
     expect(test_store.update(name: ' ')).to(eq(false))
   end
 
+  it 'validates the store name is unique before saving it' do
+    test_store = Store.create name: 'SAS Shoes'
+    test_store2 = Store.create name: 'SAS Shoes'
+    expect(test_store.save).to(eq(false))
+  end
+
   it 'ensures the length of a store name is at most 100 characters' do
     test_store = Store.new( name: "a".*(101) )
     expect(test_store.save).to(eq false )
