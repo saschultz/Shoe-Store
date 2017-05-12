507f1e1f97ef001e2cf8653b27454c0405b954fe
Sara Schultz <saschultz95@gmail.com>
2017-05-12 08:58:47 -0700

    create databases and run migrations to create tables for stores, brands, and brands_stores.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+ENV["RACK_ENV"] = "test"
+
+require "bundler/setup"
+Bundler.require :default, :test
+set :root, Dir.pwd
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+require "capybara/rspec"
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require "./app"
+
+RSpec.configure do |config|
+  config.after(:each) do
+    # Category.all.each do |d|
+    #   d.destroy
+    # end
+
+    Store.all.each do |d|
+      d.destroy
+    end
+
+    Brand.all.each do |d|
+      d.destroy
+    end
+  end
+end
