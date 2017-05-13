671be2eed0fec7d9bb70d4e6cb2af6ddb0bd632a
Sara Schultz <saschultz95@gmail.com>
2017-05-12 17:38:44 -0700

    pass integration test for adding a shoe brand to a shoe store in brand_pages_spec.rb

--- spec/brand_pages_spec.rb
+++ spec/brand_pages_spec.rb
@@ -17,4 +17,22 @@ describe 'the shoe brand path', {:type => :feature} do
     click_button 'Add'
     expect(page).to have_content 'Vans $45.00'
   end
+  it 'allows the user to add a shoe brand to a shoe store' do
+    visit '/'
+    click_link 'a shoe store'
+    fill_in 'store_name', :with => 'Toms Shoes'
+    click_button 'Add'
+    click_link 'Toms Shoes'
+    click_link 'here'
+    click_link 'here'
+    fill_in 'brand_name', :with => 'Toms'
+    fill_in 'price', :with => '48'
+    click_button 'Add'
+    click_button 'All Shoe Stores'
+    click_link 'Toms Shoes'
+    click_link 'here'
+    check 'Toms'
+    click_button 'Add'
+    expect(page).to have_content 'Toms $48.00'
+  end
 end
