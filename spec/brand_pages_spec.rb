027312760e01df9516e3aae0893eb4ce40ef4d65
Sara Schultz <saschultz95@gmail.com>
2017-05-12 17:32:04 -0700

    pass integration test for creating a new shoe brand from the all_brands path in brand_pages_spec.rb

--- spec/brand_pages_spec.rb
+++ spec/brand_pages_spec.rb
@@ -9,4 +9,12 @@ describe 'the shoe brand path', {:type => :feature} do
     click_button 'Add'
     expect(page).to have_content 'Vans $45.00'
   end
+  it 'allows the user to add a shoe brand  from the all_brands path' do
+    visit '/all_brands'
+    click_link 'here'
+    fill_in 'brand_name', :with => 'vans'
+    fill_in 'price', :with => '45'
+    click_button 'Add'
+    expect(page).to have_content 'Vans $45.00'
+  end
 end
